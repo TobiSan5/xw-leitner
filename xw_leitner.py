ffbff7ca07696ffc9a9b028da58d361b70a5f7c9
Torbjørn Wikestad <torbjorn.wikestad@gmail.com>
2024-08-31 19:14:47 +0200

    had xw_leitner.py import from xw_leitner_core

--- xw_leitner.py
+++ xw_leitner.py
@@ -1,12 +1,11 @@
 import sys
 from pathlib import Path
 
-THIS_DIR = Path(__file__).parent
-PROJ_DIR = THIS_DIR.parent
-PY_DIR = PROJ_DIR / "py"
+PROJ_DIR = Path(__file__).parent
+PY_DIR = PROJ_DIR  # / "..."
 
 if str(PY_DIR) not in sys.path:
     sys.path.insert(0, str(PY_DIR))
 
-from xl_interface import main
+from xw_leitner_core import *
 
